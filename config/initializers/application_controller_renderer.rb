e6c099812a889da257518cf88ddcb0331a483c68
20creamy21 <tsuywaga2021@gmail.com>
2021-03-24 10:31:00 +0000

    [Update]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6e88b310b72e9b3caa29987a8002ba1a95cda6d6f1cafb84a6994f556daa9db0f610fa685ad47939d18dcc2b45e3a003687027f7a6f8c3a201c042778c14e796'
